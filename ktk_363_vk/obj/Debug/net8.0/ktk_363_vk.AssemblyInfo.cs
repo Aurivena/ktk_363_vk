60eb0ff1fb8ec20499a71be85011d55512d0a2fc
Aurivena <vladkonovalov20060@gmail.com>
2024-11-15 13:29:23 +0500

    Добавление readme

--- ktk_363_vk/obj/Debug/net8.0/ktk_363_vk.AssemblyInfo.cs
+++ ktk_363_vk/obj/Debug/net8.0/ktk_363_vk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ktk_363_vk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+359b9872f7097f9fd61858a709a8f24b22c9c0c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ktk_363_vk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ktk_363_vk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
